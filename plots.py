a2fcc283e9e6b69f44c24e7141d966cbb902f319
Yaroslav Frolov <tedit.man@gmail.com>
2021-12-17 00:58:00 +0300

    исправлены ошибки

--- plots.py
+++ plots.py
@@ -1,26 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-class Constants:
-    def __init__(self):
-        """
-        Здесь параметры чёрной дыры - параметра Солнца.
-        """
-        self.m = 1.989 * (10 ** 30)  # масса чёрной дыры
-        self.c = 299792458  # скорость света
-        self.g = 6.67408 * 10 ** (-11)  # гравитационная постоянная
-        self.d = 149597870700  # расстояние до чёрной дыры
-        self.width = 1000  # ширина экрана, условность
-        self.x0 = self.width / 2  # координата x центра чёрной дыры на экране
-        self.height = 500  # высота экрана, условность
-        self.y0 = self.height / 2  # координата y центра чёрной дыры на экране
-        self.s = 1500
-        self.r_bh_screen = 50
-        self.r_bh = 696340000
-
-
-c = Constants()
+from Constants import *
 
 
 class Plot1:
@@ -74,4 +54,3 @@ class Plot3:
         plt.grid(True)
         plt.legend(loc='best', fontsize=12)
         plt.show()
-
