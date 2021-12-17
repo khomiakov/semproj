2a04d0092e8c957ca2f813fa9eabddb0c2294c6c
Yaroslav Frolov <tedit.man@gmail.com>
2021-12-17 23:38:12 +0300

    Поправил графики

--- plots.py
+++ plots.py
@@ -20,8 +20,8 @@ class Plot1:
     def draw_plot(self):
         plt.figure(figsize=(20, 7))
         plt.plot(self.x_f1, self.f1(self.x_f1 * np.pi / 180 / 3600) * 180 * 3600 / np.pi, label=r'$f(a)$')
-        plt.xlabel(r'$m(solar masses)$', fontsize=14)
-        plt.ylabel(r'$f(arc seconds)$', fontsize=14)
+        plt.xlabel(r'$a(Arc Seconds)$', fontsize=14)
+        plt.ylabel(r'$f(Arc Seconds)$', fontsize=14)
         plt.grid(True)
         plt.legend(loc='best', fontsize=12)
         plt.show()
@@ -34,14 +34,14 @@ class Plot2:
 
     def f2(self, m):
         self.zzz = None
-        alpha = np.arctan(c.r_bh_screen / c.s)
+        alpha = c.r_bh / c.d
         return - alpha / 2 + np.power((alpha / 2) ** 2 + 4 * m * c.g / c.d / (c.c ** 2), 1 / 2)
 
     def draw_plot(self):
         plt.figure(figsize=(20, 7))
         plt.plot(self.x_f2, self.f2(self.x_f2 * c.m) * 180 * 3600 / np.pi, label=r'$f(m)$')
-        plt.xlabel(r'$a(arc seconds)$', fontsize=14)
-        plt.ylabel(r'$f(arc seconds)$', fontsize=14)
+        plt.xlabel(r'$m(Solar Masses)$', fontsize=14)
+        plt.ylabel(r'$f(Arc Seconds)$', fontsize=14)
         plt.grid(True)
         plt.legend(loc='best', fontsize=12)
         plt.show()
@@ -53,15 +53,15 @@ class Plot3:
         self.zzz = None
 
     def f3(self, d):
-        alpha = np.arctan(c.r_bh_screen / c.s)
+        alpha = c.r_bh / c.d
         self.zzz = None
         return - alpha / 2 + np.power((alpha / 2) ** 2 + 4 * c.m * c.g / d / (c.c ** 2), 1 / 2)
 
     def draw_plot(self):
         plt.figure(figsize=(20, 7))
         plt.plot(self.x_f3, self.f3(self.x_f3 * c.d) * 180 * 3600 / np.pi, label=r'$f(d)$')
-        plt.xlabel(r'$a.u.$', fontsize=14)
-        plt.ylabel(r'$f(arc seconds)$', fontsize=14)
+        plt.xlabel(r'$d(10a.u.)$', fontsize=14)
+        plt.ylabel(r'$f(Arc Seconds)$', fontsize=14)
         plt.grid(True)
         plt.legend(loc='best', fontsize=12)
         plt.show()
