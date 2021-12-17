c8cd0b4bf45f88bf156e8e26e2f57190f50b59ad
kraevi <kraev.is@phystech.edu>
2021-12-17 09:00:46 +0300

    Отключен нереализованный функционал.

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ Button1 = Button()
 Button2 = Button()
 Button3 = Button()
 Button4 = Button()
-Button.create_button(Button1, screen, (40, 40, 40), c.width - 200, c.height - 100, 200, 100, 0, "Визуализация эффекта", (255, 255, 255))
+#Button.create_button(Button1, screen, (40, 40, 40), c.width - 200, c.height - 100, 200, 100, 0, "Визуализация эффекта", (255, 255, 255))
 Button.create_button(Button2, screen, (40, 40, 40), 0, c.height - 50, 70, 50, 0, "f(m)", (255, 255, 255))
 Button.create_button(Button3, screen, (40, 40, 40), 70, c.height - 50, 70, 50, 0, "f(a)", (255, 255, 255))
 Button.create_button(Button4, screen, (40, 40, 40), 140, c.height - 50, 70, 50, 0, "f(d)", (255, 255, 255))
@@ -42,6 +42,7 @@ pygame.display.update()
 
 finished = False
 clock = pygame.time.Clock()
+
 while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -49,9 +50,9 @@ while not finished:
         elif event.type == pygame.KEYDOWN and event.key == K_ESCAPE:
             finished = True
         elif event.type == MOUSEBUTTONDOWN:
-            if Button1.pressed(pygame.mouse.get_pos()):
-                print("pass")
-            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_F1) or Button2.pressed(pygame.mouse.get_pos()):
+            #if Button1.pressed(pygame.mouse.get_pos()):
+                #print("pass")
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_F1) or Button2.pressed(pygame.mouse.get_pos()):
                 plot1()
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_F2) or Button3.pressed(pygame.mouse.get_pos()):
                 plot2()
