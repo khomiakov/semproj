f7ae347add1c642ef585be6c2ef7a4c2a0ee20aa
khomiakov <khomiakov.io@phystech.edu>
2021-12-17 04:37:08 +0300

    3d sky added

---
+++ matrix_functions.py
@@ -0,0 +1,67 @@
+import math
+import numpy as np
+
+
+def transpose(a):
+    for i in range(len(a)):
+        for k in range(len(a)-i-1):
+            p = a[i][k+1+i]
+            a[i][k+1+i] = a[k+1+i][i]
+            a[k+1+i][i] = p
+    return a
+
+            
+def translate(pos):
+    tx, ty, tz = pos
+    return np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [tx, ty, tz, 1]
+    ])
+
+
+def rotate_x(a):
+    return np.array([
+        [1, 0, 0, 0],
+        [0, math.cos(a), math.sin(a), 0],
+        [0, -math.sin(a), math.cos(a), 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def rotate_y(a):
+    return np.array([
+        [math.cos(a), 0, -math.sin(a), 0],
+        [0, 1, 0, 0],
+        [math.sin(a), 0, math.cos(a), 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def rotate_z(a):
+    return np.array([
+        [math.cos(a), math.sin(a), 0, 0],
+        [-math.sin(a), math.cos(a), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def scale(n):
+    return np.array([
+        [n, 0, 0, 0],
+        [0, n, 0, 0],
+        [0, 0, n, 0],
+        [0, 0, 0, 1]
+    ])
+
+def generate_3D_sky():
+    a = np.random.sample((3000, 4))
+    r = 1
+    for i in range(len(a)):
+        a[i][2] = (a[i][2] * 2 - 1)*r
+        a[i][1] = (r**2 - a[i][2]**2)**0.5 * np.sin(a[i][0] * 2*np.pi)
+        a[i][0] = (r**2 - a[i][2]**2)**0.5 * np.cos(a[i][0] * 2*np.pi)
+        a[i][3] = 1
+    return a
