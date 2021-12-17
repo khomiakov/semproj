fe20b00a619da0b258e40cd08df6d5b5bb743889
kraevi <kraev.is@phystech.edu>
2021-12-17 10:13:55 +0300

    Доработан модуль.

--- 2d_editor.py
+++ 2d_editor.py
@@ -9,15 +9,19 @@
 
 WHITE = 16777215
 
+zone_of_influence = pygame.Surface((2*c.r_bh_screen, 2*c.r_bh_screen)
+
+
 def input1():
         """"В зоне действия эффекта ищет светящиеся объекты.
         Из массива всех пикселей оставляет только их.""""
         global glowing_dots
-        ar = pygame.PixelArray(zone_of_influence)
+        ar = pygame.PixelArray(screen)
         glowing_dots = []
         for i in range(len(ar)):
             for j in range(len(ar[0])): 
-                if ar[i,j] == WHITE:
+                if ar[i,j] == WHITE and (i > c.x0 - 2*c.r_bh_screen) and i < (c.x0 + 2*c.r_bh_screen)
+                and j > (c.y0 - 2*c.r_bh_screen) and j < (c.y0 + 2*c.r_bh_screen):
                 glowing_dots.append([i,j])
                 
 def output():
@@ -27,8 +31,8 @@ def output():
     for k in range(len(glowing_dots)):
         ar[glowing_dots[k][0], glowing_dots[k][1]] = (255, 255, 255)
     del ar
-    screen.blit(zone_of_influence, (,))
-    circle(screen, (0,0,0), (LENGTH/2, HEIGHT/2), black_hole_R)
+    screen.blit(zone_of_influence, (c.x0 - 2*c.r_bh_screen, c.y0 - 2*c.r_bh_screen))
+    circle(screen, (0,0,0), (c.x0, c.y0), c.r_bh_screen)
     pygame.display.flip()
     
 
