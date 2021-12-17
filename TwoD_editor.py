b6ab3ffb6101acaf9c12a5d4071173b1b0ecb38a
Yaroslav Frolov <tedit.man@gmail.com>
2021-12-17 10:58:48 +0300

    patch

--- TwoD_editor.py
+++ TwoD_editor.py
@@ -10,11 +10,11 @@ import pygame
 from Constants import *
 
 WHITE = 16777215
-
+glowing_dots = []
 zone_of_influence = pygame.Surface((2*c.r_bh_screen, 2*c.r_bh_screen))
 
 
-def input1():
+def input1(screen):
     """"В зоне действия эффекта ищет светящиеся объекты.
     Из массива всех пикселей оставляет только их."""
     global glowing_dots
@@ -24,9 +24,10 @@ def input1():
         for j in range(len(ar[0])): 
             if ar[i,j] == WHITE and (i > c.x0 - 2*c.r_bh_screen) and i < (c.x0 + 2*c.r_bh_screen) \
             and j > (c.y0 - 2*c.r_bh_screen) and j < (c.y0 + 2*c.r_bh_screen):
-                glowing_dots.append([i,j])
-                
-def output():
+                glowing_dots.append([i, j])
+
+
+def output(screen):
     """Отрисовывает обновленный участок."""
     ar = pygame.PixelArray(zone_of_influence)
     ar[:] = 0
@@ -34,7 +35,7 @@ def output():
         ar[glowing_dots[k][0], glowing_dots[k][1]] = (255, 255, 255)
     del ar
     screen.blit(zone_of_influence, (c.x0 - 2*c.r_bh_screen, c.y0 - 2*c.r_bh_screen))
-    circle(screen, (0,0,0), (c.x0, c.y0), c.r_bh_screen)
+    pygame.draw.circle(screen, (0, 0, 0), (c.x0, c.y0), c.r_bh_screen)
     pygame.display.flip()
     
 
