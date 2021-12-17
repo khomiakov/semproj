ac2026b4668fb4158e59610801b513f4cc7c5472
kraevI <kraev.is@phystech.edu>
2021-12-17 08:49:55 +0300

    Мелкие изменения. Параметры окна привязаны к константам.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from plots import *
 from pygame.locals import *
 from buttons import *
+from Constants import *
 
 pygame.init()
 
@@ -19,23 +20,23 @@ def plot3():
     p.draw_plot()
 
 
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((c.width, c.height))
 pygame.display.set_caption("Гравитационное линзирование")
 Button1 = Button()
 Button2 = Button()
 Button3 = Button()
 Button4 = Button()
-Button.create_button(Button1, screen, (40, 40, 40), 600, 300, 200, 100, 0, "Визуализация эффекта", (255, 255, 255))
-Button.create_button(Button2, screen, (40, 40, 40), 0, 350, 70, 50, 0, "f(a)", (255, 255, 255))
-Button.create_button(Button3, screen, (40, 40, 40), 70, 350, 70, 50, 0, "f(m)", (255, 255, 255))
-Button.create_button(Button4, screen, (40, 40, 40), 140, 350, 70, 50, 0, "f(d)", (255, 255, 255))
+Button.create_button(Button1, screen, (40, 40, 40), c.width - 200, c.height - 100, 200, 100, 0, "Визуализация эффекта", (255, 255, 255))
+Button.create_button(Button2, screen, (40, 40, 40), 0, c.height - 50, 70, 50, 0, "f(m)", (255, 255, 255))
+Button.create_button(Button3, screen, (40, 40, 40), 70, c.height - 50, 70, 50, 0, "f(a)", (255, 255, 255))
+Button.create_button(Button4, screen, (40, 40, 40), 140, c.height - 50, 70, 50, 0, "f(d)", (255, 255, 255))
 pygame.display.flip()
 
 
 fontObj = pygame.font.Font('freesansbold.ttf', 10)
 textSurfaceObj = fontObj.render('Нажмите для построения графиков:', True, (255,255,255))
 textRectObj = textSurfaceObj.get_rect()
-textRectObj.center = (105, 330)    
+textRectObj.center = (105, c.height - 70) 
 screen.blit(textSurfaceObj, textRectObj)
 pygame.display.update()
 
