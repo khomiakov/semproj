bb03b9cf8d7bd88e6d5a280dee867ca07f738b5a
khomiakov <khomiakov.io@phystech.edu>
2021-12-17 10:31:07 +0300

    added description to camera

--- camera.py
+++ camera.py
@@ -1,13 +1,25 @@
+"""Этот модуль отвечает за экран наблюдателя, его систему координат,
+и его положение в пространстве.
+"""
 import pygame as pg
 from matrix_functions import *
 
+
 class Camera:
     def __init__(self, render, position):
+        """
+        :self.position - начало координат наблюдателя в ЛСО.
+        :self.forward, self.up, self.right - базисные вектора СО наблюдателя в ЛСО, z, y, x соответственно.
+        :self.h_fov, self.v_fov - углы обзора по вертикали и горизонтали соответственно.
+        :self.near_plane, self.far_plane - расстояния до плоскостей отсечения прорисовки.
+        :self.moving_speed - скорость движения наблюдателя
+        :self.rotation_speed - скорость поворота наблюдателя
+        """
         self.render = render
         self.position = np.array([*position, 1.0])
-        self.forward = np.array([0, 0, 1, 1]) #z
-        self.up = np.array([0, 1, 0, 1]) #y
-        self.right = np.array([1, 0, 0, 1]) #x
+        self.forward = np.array([0, 0, 1, 1])  # z
+        self.up = np.array([0, 1, 0, 1])  # y
+        self.right = np.array([1, 0, 0, 1])  # x
         self.h_fov = math.pi / 3
         self.v_fov = self.h_fov * (render.HEIGHT / render.WIDTH)
         self.near_plane = 0.1
@@ -16,6 +28,8 @@ class Camera:
         self.rotation_speed = 0.015
 
     def control(self):
+        """Функция управления движением наблюдателя через клавиатуру
+        """
         key = pg.key.get_pressed()
         if key[pg.K_a]:
             self.position -= self.right * self.moving_speed
@@ -35,32 +49,40 @@ class Camera:
         if key[pg.K_RIGHT]:
             self.camera_yaw(self.rotation_speed)
         if key[pg.K_UP]:
-            self.camera_pitch(-self.rotation_speed) 
+            self.camera_pitch(-self.rotation_speed)
         if key[pg.K_DOWN]:
             self.camera_pitch(self.rotation_speed)
 
     def camera_yaw(self, angle):
+        """Функция поворота наблюдателя вокруг оси Y ЛСО
+        """
         rotate = rotate_y(angle)
         self.forward = self.forward @ rotate
         self.right = self.right @ rotate
         self.up = self.up @ rotate
 
     def camera_pitch(self, angle):
+        """Функция поворота наблюдателя вокруг оси X ЛСО
+        """
         rotate = rotate_x(angle)
-        self.forward = self.forward @ rotate  #@ self.camera_matrix()
+        self.forward = self.forward @ rotate  # @ self.camera_matrix()
         self.right = self.right @ rotate
         self.up = self.up @ rotate
 
     def translate_matrix(self):
+        """Матрица перемещения
+        """
         x, y, z, w = self.position
         return np.array([
             [1, 0, 0, 0],
-            [0, 1, 0, 0],   #[0, 1, 0, 1]
+            [0, 1, 0, 0],  # [0, 1, 0, 1]
             [0, 0, 1, 0],
             [-x, -y, -z, 1]
         ])
 
     def rotate_matrix(self):
+        """Матрица вращения
+        """
         rx, ry, rz, w = self.right
         fx, fy, fz, w = self.forward
         ux, uy, uz, w = self.up
@@ -72,4 +94,6 @@ class Camera:
         ])
 
     def camera_matrix(self):
+        """Комбинированнная матрица вращения и перемещения
+        """
         return self.translate_matrix() @ self.rotate_matrix()
