aadfae0f650149cdebeb1203592ef70c492b541a
khomiakov <khomiakov.io@phystech.edu>
2021-12-20 08:34:18 +0300

    added equator

--- matrix_functions.py
+++ matrix_functions.py
@@ -57,12 +57,4 @@ def scale(n):
     ])
 
 
-def generate_3d_sky():
-    a = np.random.sample((3000, 4))
-    r = 1
-    for i in range(len(a)):
-        a[i][2] = (a[i][2] * 2 - 1)*r
-        a[i][1] = (r**2 - a[i][2]**2)**0.5 * np.sin(a[i][0] * 2*np.pi)
-        a[i][0] = (r**2 - a[i][2]**2)**0.5 * np.cos(a[i][0] * 2*np.pi)
-        a[i][3] = 1
-    return a
+
--- threeD_main.py
+++ threeD_main.py
@@ -21,12 +21,18 @@ class VisualEngine:
         self.camera = Camera(self, [0, 0, 0])
         self.projection = Projection(self)
         self.object = Object3D(self, generate_3d_sky())
+        self.equator = Object3D(self, gen_equator_vert(), gen_equator_face())
+        self.equator.draw_vertexes = False
+        self.equator.font = pg.font.SysFont('Arial', 20, bold=True)
+        self.equator.color_faces = [(pg.Color('white'), face) for face in self.equator.faces]
+        self.equator.label = 10* 'equator                                           '
         self.axes = Axes(self)
 
     def draw(self):
         self.screen.fill(c.sky_color)
         self.object.draw()
         self.axes.draw()
+        self.equator.draw()
 
     def run(self):
         self.draw()
